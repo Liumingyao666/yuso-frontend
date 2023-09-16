926974d20d3cdf923ea355bf526d5412461a2339
Liumingyao666 <1806859182@qq.com>
2023-09-16 15:13:30 +0800

    add 主页搜索，记录搜索状态

---
+++ src/components/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+  <a-list item-layout="horizontal" :data-source="props.postList">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-list-item-meta :description="item.content">
+          <template #title>
+            <a href="https://www.antdv.com/">{{ item.title }}</a>
+          </template>
+          <template #avatar>
+            <a-avatar :src="gege" />
+          </template>
+        </a-list-item-meta>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import gege from "../assets/gege.jpg";
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+  postList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  postList: () => [],
+});
+</script>
+
+<style scoped>
+.gege {
+  width: 800px;
+  height: 500px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchParams.text"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+    <MyDivider />
+    <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
+      <a-tab-pane key="post" tab="文章">
+        <PostList :post-list="postList" />
+      </a-tab-pane>
+      <a-tab-pane key="picture" tab="图片">
+        <PictureList />
+      </a-tab-pane>
+      <a-tab-pane key="user" tab="用户">
+        <UserList :user-list="userList" />
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import PostList from "@/components/PostList.vue";
+import UserList from "@/components/UserList.vue";
+import PictureList from "@/components/PictureList.vue";
+import MyDivider from "@/components/MyDivider.vue";
+import { useRoute, useRouter } from "vue-router";
+import myAxios from "@/plugins/MyAxios";
+
+const userList = ref([]);
+
+myAxios.post("user/list/page/vo", {}).then((res: any) => {
+  userList.value = res.records;
+});
+
+const postList = ref([]);
+
+myAxios.post("post/list/page/vo", {}).then((res: any) => {
+  postList.value = res.records;
+});
+
+const route = useRoute();
+const router = useRouter();
+const activeKey = route.params.category;
+
+const initSearchParams = {
+  text: "",
+  pageSize: 10,
+  pageNum: 1,
+};
+
+const searchParams = ref(initSearchParams);
+
+watchEffect(() => {
+  searchParams.value = {
+    ...initSearchParams,
+    text: route.query.text,
+  } as any;
+});
+
+const onSearch = (value: string) => {
+  alert(value);
+  router.push({
+    query: searchParams.value,
+  });
+};
+
+const onTabChange = (key: string) => {
+  router.push({
+    path: `/${key}`,
+    query: searchParams.value,
+  });
+};
+</script>
